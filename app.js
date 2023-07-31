2241af59980b8fb5de245057cca4cbd3b62af2e9
yonatan taub <yonatantaub36@gmail.com>
2023-07-31 14:36:06 +0300

    cors

--- app.js
+++ app.js
@@ -10,16 +10,7 @@ const loggersService = require("./utils/loggers/loggerServise");
 
 var app = express();
 
-app.use(
-  cors({
-    origin: [
-      "http://127.0.0.1:5500",
-      "http://localhost:3000",
-      "http://localhost:8181",
-    ],
-    optionsSuccessStatus: 200,
-  })
-);
+app.use(cors());
 app.use(loggersService.logger); // Specify the "combined" format
 
 app.use(express.json());
