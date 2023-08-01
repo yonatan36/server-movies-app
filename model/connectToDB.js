588bb2fc2faac6a1925f857f9f43b4c383c48a54
yonatan taub <yonatantaub36@gmail.com>
2023-08-01 11:03:02 +0300

    cards

--- model/connectToDB.js
+++ model/connectToDB.js
@@ -3,4 +3,6 @@ const mongoose = require("mongoose");
 
 console.log("con str", config.get("dbConfig.url"));
 
+// module.exports = mongoose.connect(config.get("dbConfig.url"));
+
 module.exports = mongoose.connect(`${process.env.ORIGIN}`);
